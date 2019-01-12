450b7740f37c41a51f278276188fb2164fed8ce3
Dylan Steele <dylansteele57@gmail.com>
2019-01-11 21:21:20 -0500

    Added better catching logic before implementing random pokemon and item methods.

--- src/catch.js
+++ src/catch.js
@@ -3,10 +3,30 @@ const {
     GenerateQuestionObj
 } = require('./q-builder');
 
-function AttemptToCatch() {
+function AttemptToCatch(i) {
+    i = i ? i : 0;
     inquirer.prompt([GenerateQuestionObj()]).then((answers) => {
-        console.log(`catch: ${answers.catch}`);
+        if (!answers.catch.includes('N')) {
+            if (!CheckLuck()) {
+                if (i != 3) {
+                    AttemptToCatch(i++);
+                }
+                console.log("Couldn't find anything yet");
+                return;
+            }
+            console.log('Nice! you found something!');
+        }
     });
+}
+
+function CheckLuck() {
+    const _maxNum = 50000;
+    var randNum = Math.floor(Math.random() * _maxNum) + 1;
+    console.log(randNum);
+    if (randNum % 2 || randNum % 7) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = {
