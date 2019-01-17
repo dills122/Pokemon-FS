2ed7b265beb059b67403915a471932b9b3458824
Dylan Steele <dylansteele57@gmail.com>
2019-01-16 21:18:37 -0500

    Added basic test to avoid test script failing.

---
+++ test/init-tests/catchem-tests.spec.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const mocha = require('mocha');
+const chai = require('chai');
+const catchem = require('../../catchem');
+const expect = chai.expect;
+
+describe('Catchem', function () {
+    it('it should be a void function', function () {
+        expect(catchem).to.be.an('object');
+        expect(typeof catchem.init).to.equal('function');
+    });
+});
