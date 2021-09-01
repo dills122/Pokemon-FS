a22941626276b2c22f951d8139bab0af82b29ba5
Dylan Steele <dylansteele57@gmail.com>
2021-09-01 19:21:32 -0400

    added more tests

--- src/classes/search-history.ts
+++ src/classes/search-history.ts
@@ -6,27 +6,32 @@ const _maxHistorySize = 500;
 export default class SearchHistory {
   private dataFileObject: DataFile;
   private searchHistory: string[];
+
   async init(): Promise<void> {
     this.dataFileObject = new DataFile();
     await this.dataFileObject.init();
     this.searchHistory = this.dataFileObject.getSearchHistory();
   }
+
   public ReviewSearchHistory(): boolean {
     this.CleanUpSearchHistory();
     const currDirHash = this.HashSearchEntry();
     return this.CheckHistory(currDirHash);
   }
+
   public async AddSearchEntry(): Promise<void> {
     this.searchHistory.push(this.HashSearchEntry());
     await this.dataFileObject.updateFile({
       searchHistoryUpdates: this.searchHistory,
     });
   }
+
   private HashSearchEntry() {
     const hours = new Date().getHours();
     const currDir = process.cwd();
     return hasha(currDir + hours);
   }
+
   private CleanUpSearchHistory() {
     const historyLength = this.searchHistory.length;
     if (historyLength >= _maxHistorySize) {
@@ -37,6 +42,7 @@ export default class SearchHistory {
       );
     }
   }
+
   private CheckHistory(searchEntryHash: string) {
     let CanSearch = true;
     this.searchHistory.some((value) => {
---
+++ test/catch/catch.spec.ts
@@ -0,0 +1,152 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+import Sinon from "sinon";
+import Catch from "../../src/catch";
+import inquirer from "inquirer";
+// import Question from "../../src/question-builder";
+import * as GetRandomPokemon from "../../src/get-random-pokemon";
+import { PokemonInventory } from "../../src/classes/inventory";
+
+describe("Catch::", () => {
+  let sandbox: Sinon.SinonSandbox;
+  let stubs: any = {};
+
+  beforeEach(() => {
+    sandbox = Sinon.createSandbox();
+    stubs.DisplayPromptStub = sandbox
+      .stub(Catch, <any>"DisplayPrompt")
+      .resolves();
+    stubs.CatchStub = sandbox.stub(Catch, <any>"Catch").resolves();
+    stubs.inquirerPromptStub = sandbox.stub(inquirer, "prompt");
+    sandbox.stub(console, "error").returns();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe("Attempt to Catch", () => {
+    it("Should execute happy path, have luck", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      await Catch.AttemptToCatch();
+      expect(stubs.DisplayPromptStub.callCount).to.equal(1);
+    });
+    it("Should execute happy path, no luck", async () => {
+      stubs.CheckLuckStub = sandbox
+        .stub(Catch, <any>"CheckLuck")
+        .returns(false);
+      await Catch.AttemptToCatch();
+      expect(stubs.DisplayPromptStub.callCount).to.equal(0);
+    });
+    it("Display Prompt thorws", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.DisplayPromptStub.rejects(Error("err"));
+      try {
+        await Catch.AttemptToCatch();
+        expect(true).equal(false);
+      } catch (err) {
+        expect(err.message).equal("err");
+        expect(stubs.DisplayPromptStub.callCount).to.equal(1);
+      }
+    });
+  });
+
+  describe("Display Prompt", () => {
+    beforeEach(() => {
+      stubs.DisplayPromptStub.restore();
+    });
+    it("Should go through happy path, answer yes", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "Yes",
+      });
+      await Catch.AttemptToCatch();
+      expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+      expect(stubs.CatchStub.callCount).to.equal(1);
+    });
+    it("Should go through happy path, answer no", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "No",
+      });
+      await Catch.AttemptToCatch();
+      expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+      expect(stubs.CatchStub.callCount).to.equal(0);
+    });
+  });
+
+  describe("Catch", () => {
+    beforeEach(() => {
+      stubs.DisplayPromptStub.restore();
+      stubs.CatchStub.restore();
+      stubs.GetRandomPokemonStub = sandbox
+        .stub(GetRandomPokemon, "default")
+        .resolves("NAME");
+      stubs.PokemonInventoryInitStub = sandbox
+        .stub(PokemonInventory.prototype, "init")
+        .resolves();
+      stubs.PokemonInventoryAddStub = sandbox
+        .stub(PokemonInventory.prototype, "add")
+        .resolves();
+    });
+    it("Should go through happy path", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "Yes",
+      });
+      await Catch.AttemptToCatch();
+      expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+      expect(stubs.GetRandomPokemonStub.callCount).to.equal(1);
+      expect(stubs.PokemonInventoryInitStub.callCount).to.equal(1);
+      expect(stubs.PokemonInventoryAddStub.callCount).to.equal(1);
+    });
+    it("Get Random Pokemon Throws", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "Yes",
+      });
+      stubs.GetRandomPokemonStub.rejects(Error("err"));
+      try {
+        await Catch.AttemptToCatch();
+        expect(true).equal(false);
+      } catch (err) {
+        expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+        expect(stubs.GetRandomPokemonStub.callCount).to.equal(1);
+        expect(stubs.PokemonInventoryInitStub.callCount).to.equal(0);
+        expect(stubs.PokemonInventoryAddStub.callCount).to.equal(0);
+      }
+    });
+    it("Pokemon Inv Init Throws", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "Yes",
+      });
+      stubs.PokemonInventoryInitStub.rejects(Error("err"));
+      try {
+        await Catch.AttemptToCatch();
+        expect(true).equal(false);
+      } catch (err) {
+        expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+        expect(stubs.GetRandomPokemonStub.callCount).to.equal(1);
+        expect(stubs.PokemonInventoryInitStub.callCount).to.equal(1);
+        expect(stubs.PokemonInventoryAddStub.callCount).to.equal(0);
+      }
+    });
+    it("Pokemon Add to Inv Throws", async () => {
+      stubs.CheckLuckStub = sandbox.stub(Catch, <any>"CheckLuck").returns(true);
+      stubs.inquirerPromptStub.resolves({
+        catch: "Yes",
+      });
+      stubs.PokemonInventoryAddStub.rejects(Error("err"));
+      try {
+        await Catch.AttemptToCatch();
+        expect(true).equal(false);
+      } catch (err) {
+        expect(stubs.inquirerPromptStub.callCount).to.equal(1);
+        expect(stubs.GetRandomPokemonStub.callCount).to.equal(1);
+        expect(stubs.PokemonInventoryInitStub.callCount).to.equal(1);
+        expect(stubs.PokemonInventoryAddStub.callCount).to.equal(1);
+      }
+    });
+  });
+});
